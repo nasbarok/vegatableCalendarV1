a2261db8669963a1b161ae871c3c11c09614d59b
naoua <nouahit@umanis.com>
2020-05-19 14:47:56 +0200

    swipp

--- app/src/main/java/com/nasbarok/vegatablecalendarv1/utils/VegetableCalendarNotify.java
+++ app/src/main/java/com/nasbarok/vegatablecalendarv1/utils/VegetableCalendarNotify.java
@@ -14,17 +14,22 @@ import java.util.Date;
 public class VegetableCalendarNotify {
 
     public void createNotifyCalendar(Context context, Calendar beginTime, Calendar endTime, String location, String description, String title){
-        Intent intent = new Intent(Intent.ACTION_INSERT)
-                .setData(CalendarContract.Events.CONTENT_URI)
-                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
-                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
-                .putExtra(CalendarContract.Events.TITLE, title)
-                .putExtra(CalendarContract.Events.DESCRIPTION, description)
-                .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
-                .putExtra(CalendarContract.Events.EVENT_COLOR_KEY, Color.RED)
-                .putExtra(CalendarContract.Reminders.MINUTES, 5)
-                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
-                .putExtra(Intent.EXTRA_EMAIL, "na.ouahit@gmail.com");
+        int argbValue = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            argbValue = Color.valueOf(context.getResources().getColor(R.color.table_color_iseeding)).toArgb();
+        }
+        Intent intent = null;
+
+            intent = new Intent(Intent.ACTION_INSERT)
+                    .setData(CalendarContract.Events.CONTENT_URI)
+                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                    .putExtra(CalendarContract.Events.TITLE, title)
+                    .putExtra(CalendarContract.Events.DESCRIPTION, description)
+                    .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+                    .putExtra(CalendarContract.Events.EVENT_COLOR_KEY, argbValue)
+                    .putExtra(CalendarContract.Reminders.MINUTES, 20)
+                    .putExtra(Intent.EXTRA_EMAIL, "na.ouahit@gmail.com");
 
         context.startActivity(intent);
     }
