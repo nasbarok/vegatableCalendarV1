cddc8fd3d452a0fac5c4d1b1f95058f2505012de
naoua <nouahit@umanis.com>
2021-03-28 16:58:06 +0200

    moon works

---
+++ app/src/main/java/com/nasbarok/vegatablecalendarv1/utils/SunAndMoon.java
@@ -0,0 +1,16 @@
+package com.nasbarok.vegatablecalendarv1.utils;
+
+import java.util.Date;
+
+public class SunAndMoon {
+    private double latitude;
+    private double longitude;
+    private Date startDate;
+
+    public SunAndMoon(double latitude,double longitude){
+        latitude = latitude;
+        longitude = longitude;
+    }
+
+
+}
